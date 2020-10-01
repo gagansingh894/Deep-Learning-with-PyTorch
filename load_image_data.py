54164209e397f7c5cfb3e79c7e5e3f3e51210b24
gagansingh894 <gds31.gagandeepsingh@gmail.com>
2020-10-02 00:24:13 +0530

    New Learning

--- load_image_data.py
+++ load_image_data.py
@@ -2,26 +2,26 @@ import torch
 from torchvision import datasets, transforms
 
 data_dir = 'Cat_Dog_data/train'
-transforms = transforms.Compose([transforms.Resize(225),
+transform = transforms.Compose([transforms.Resize(225),
                                 transforms.CenterCrop(224),
-                               transforms.ToTensor()])
+                                transforms.ToTensor()])
 
-dataset = datasets.ImageFolder(data_dir, transform=transforms)
+dataset = datasets.ImageFolder(data_dir, transform=transform)
 data_loader = torch.utils.data.DataLoader(dataset, batch_size=32, shuffle=True)
 
 
 # Data Augmentation
-train_transforms = transforms.Compose([transforms.RandomRotation(30),
-                                       transforms.RandomResizedCrop(224),
-                                       transforms.RandomHorizontalFlip(),
-                                       transforms.ToTensor()])
-
-test_transforms = transforms.Compose([transforms.Resize(255),
-                                      transforms.CenterCrop(224),
+train_transform = transforms.Compose([transforms.RandomRotation(30),
+                                      transforms.RandomResizedCrop(224),
+                                      transforms.RandomHorizontalFlip(),
                                       transforms.ToTensor()])
 
-train_data = datasets.ImageFolder(data_dir + '/train', transform=train_transforms)
-test_data = datasets.ImageFolder(data_dir + '/test', transform=test_transforms)
+test_transform = transforms.Compose([transforms.Resize(255),
+                                     transforms.CenterCrop(224),
+                                     transforms.ToTensor()])
+
+train_data = datasets.ImageFolder(data_dir + '/train', transform=train_transform)
+test_data = datasets.ImageFolder(data_dir + '/test', transform=test_transform)
 
 train_loader = torch.utils.data.DataLoader(train_data, batch_size=32)
 test_loader = torch.utils.data.DataLoader(test_data, batch_size=32)
